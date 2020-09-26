278d98b0d143ed542d5ec9d0c9a15200f5f1ee3d
nabil mokhtar <nabilmokhtar15@gmail.com>
2020-09-26 18:02:34 +0200

    services

--- src/main/java/com/ITAmbition/Version1/Repository/CateogryRepo.java
+++ src/main/java/com/ITAmbition/Version1/Repository/CateogryRepo.java
@@ -1,9 +1,9 @@
 package com.ITAmbition.Version1.Repository;
-import com.ITAmbition.Version1.Entity.SubCategoryEntity;
+
+import com.ITAmbition.Version1.Entity.CateogryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubCategoryRepo extends JpaRepository<SubCategoryEntity,Integer>{
-
+public interface CateogryRepo extends JpaRepository<CateogryEntity,Integer> {
 }
